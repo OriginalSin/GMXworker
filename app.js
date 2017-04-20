17bb1f71b71191d8a0115606a464bd3297c96a31
OriginalSin <saleks@scanex.ru>
2017-04-20 17:17:29 +0300

    https

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([55.73, 37.59], 5);
 
-var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('https://tilessputnik.ru/{z}/{x}/{y}.png', {
 	maxZoom: 17,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 }).addTo(map);
